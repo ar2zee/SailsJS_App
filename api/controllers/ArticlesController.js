0b47f517dcd5d1f29a40f46db1a4a66663ec2eb6
Oganesyan Artur <arturris@mail.ru>
2018-01-09 15:11:03 -0600

    Article List & Add Pages

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
-	
+	list: function(req, res) {
+		 Articles.find({}).exec(function(err,articles){
+		 	if(err) {
+		 		res.send(500, {error: 'DB Error'})
+		 	} else {
+		 		res.view('list', {articles:articles})
+		 	}
+		 })
+	},
+	add: function(req, res){
+		res.view('add')
+	},
+	create: function(req, res){
+		const title = req.body.title;
+		const body = req.body.body;
+
+		Articles.create({title: title, body: body}).exec(function(err) {
+			if(err) {
+				res.send(500, {error: 'DB Error'})
+			} 
+			res.redirect('/articles/list');
+		})
+	}
 };
 
